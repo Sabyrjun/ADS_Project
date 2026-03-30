3e9997c0de550f0a1292bdf82ca6f2e22d1d98a7
Retr0 <sabyr1232501@gmail.com>
2026-03-30 16:04:09 +0500

    commit

---
+++ src/main/java/com/example/ads_project/AdsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ads_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdsProjectApplication.class, args);
+    }
+
+}
