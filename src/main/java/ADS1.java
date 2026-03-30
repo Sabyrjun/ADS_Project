54daccb897c01656c62cf66b5f6d40a88eee5c58
Retr0 <sabyr1232501@gmail.com>
2026-03-30 20:59:18 +0500

    commit

--- src/main/java/ADS1.java
+++ src/main/java/ADS1.java
@@ -1,18 +1,56 @@
+/*
 import java.util.Scanner;
 public class ADS1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter an integer n: ");
+        System.out.print("Sample Input: n = ");
         int n = input.nextInt();
+        System.out.print("Sample Output: ");
         int result = sumSquares(n);
-        System.out.println("The sum of the first squares" + n + "integer: " + result);
+        System.out.println(" = " + result);
         input.close();
     }
-
     public static int sumSquares(int n) {
-        if (n <= 1){
+        if (n <= 1) {
+            System.out.print("1^2");
             return 1;
         }
-        return (n*n)+sumSquares(n-1);
+        int sum = sumSquares(n - 1);
+        System.out.print(" + " + n + "^2");
+        return (n * n) + sum;
     }
 }
+*/
+
+/*
+import  java.util.Scanner;
+public class ADS1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите общий размер масива: ");
+        int size = sc.nextInt();
+        int[] numbers = new int[size];
+
+        System.out.println("Введите числа через пробел или Enter:");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        System.out.print("Сколько первых цифр сложить?: ");
+        int n = sc.nextInt();
+        int result = sumArray(numbers, n);
+        System.out.println("Сумма первых " + n + " элементов " + result);
+        sc.close();
+    }
+
+
+    public static int sumArray(int[] arr, int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        return arr[n - 1] + sumArray(arr, n - 1);
+    }
+}
+*/
+
+
