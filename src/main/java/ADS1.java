0f8bf88e5939ffd4a7943e8c088c29fc718799c2
Retr0 <sabyr1232501@gmail.com>
2026-03-30 16:26:12 +0500

    commit

--- src/main/java/ADS1.java
+++ src/main/java/ADS1.java
@@ -1,2 +1,18 @@
-public class ADS1 {0000
+import java.util.Scanner;
+public class ADS1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer n: ");
+        int n = input.nextInt();
+        int result = sumSquares(n);
+        System.out.println("The sum of the first squares" + n + "integer: " + result);
+        input.close();
+    }
+
+    public static int sumSquares(int n) {
+        if (n <= 1){
+            return 1;
+        }
+        return (n*n)+sumSquares(n-1);
+    }
 }
